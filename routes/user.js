0b51f2519129294c75ddd00d21d6064e04d5e947
jeonghwan <ej88ej@gmail.com>
2015-06-11 15:21:49 +0900

    Add express-jwt

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', auth.isAuthenticated(), function(req, res) {
-  res.send('respond with a resource');
+  res.send(req.user);
 });
 
 module.exports = router;
